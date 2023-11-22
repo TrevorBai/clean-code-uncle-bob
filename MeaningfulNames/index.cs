3dbd6d32b419a404605eb1c90ebacec94de7e49a
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2023-11-22 00:26:24 -0500

    Create index.cs

---
+++ MeaningfulNames/index.cs
@@ -0,0 +1,7 @@
+/*
+ * 1. Whenever you would like to name a meaningful variable/method/class name, ignore a/an/the even though if you write it
+ *    in a literal sentence you would need use them.
+ * 2. For example, byte* pointerToFirst. If you write it in literal, it would be pointer to the first (element). But in coding
+ *    variable name, you can't use the. Take another example, method name IsStraightLine, not IsAStraightLine.
+ *
+ */
