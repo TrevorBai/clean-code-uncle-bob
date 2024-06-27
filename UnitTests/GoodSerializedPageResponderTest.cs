684114e0e8eabac83515d07c35df0684debcc9c8
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2024-06-27 17:28:06 -0400

    Add design pattern comments

--- UnitTests/GoodSerializedPageResponderTest.cs
+++ UnitTests/GoodSerializedPageResponderTest.cs
@@ -2,6 +2,19 @@
 // human-readable methods name which is very helpful even for a guy who doesn't know
 // web tech.
 
+// Without using fancy terms, I guess this is the SINGLE key factor or art of programming,
+// i.e. make the code human readable, encapsulate details away, and build a hierarchical
+// code structure, which is what I am striving for as my day job.
+
+// There is a design pattern or a code pattern used here for writing a unit test, which is
+// build-operate-check pattern. Tbh, I used it to write my test just without knowing the name
+// of it.
+
+// It has three parts:
+// 1. build up the test data (model).
+// 2. operate on the test data (model).
+// 3. checks the operation yielded the expected results.
+
 public void TestGetPageHierarchyAsXml() 
 {
     MakePages("PageOne", "PageOne.ChildOne", "PageTwo");
