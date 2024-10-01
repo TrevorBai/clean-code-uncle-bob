b26b4fc0c966fc89cbb03f053bfeb829dd36727f
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2024-10-01 16:38:36 -0400

    Add remaining methods of PrimeGenerator

--- Classes/PrintPrimesGood/PrimeGenerator.cs
+++ Classes/PrintPrimesGood/PrimeGenerator.cs
@@ -20,7 +20,52 @@ public class PrimeGenerator
         _multiplesOfPrimeFactors.Add(2);
     }
 
-    // ...
+    private static void CheckOddNumbersForSubsequentPrimes()
+    {
+        int primeIndex = 1;
+        for (int candidate = 3; primeIndex < _primes.Length; candidate += 2)
+        {
+            if (IsPrime(candidate)) _primes[primeIndex++] = candidate;
+        }    
+    }
 
-    
+    private static bool IsPrime(int candidate)
+    {
+        if (IsLeastRelevantMultipleOfNextLargerPrimeFactor(candidate))
+        {
+            _multiplesOfPrimeFactors.Add(candidate);
+            return false;
+        }
+        return IsNotMultipleOfAnyPreviousPrimeFactor(candidate);    
+    }
+
+    private static bool IsLeastRelevantMultipleOfNextLargerPrimeFactor(int candidate)
+    {
+        int nextLargerPrimeFactor = _primes[_multiplesOfPrimeFactors.Count];
+        int leastRelevantMultiple = nextLargerPrimeFactor * nextLargerPrimeFactor;
+        return candidate == leastRelevantMultiple;
+    }
+
+    private static bool IsNotMultipleOfAnyPreviousPrimeFactor(int candidate)
+    {
+        for (int n = 1; n < _multiplesOfPrimeFactors.Count; n++)
+        {
+            if (IsMultipleOfNthPrimeFactor(candidate, n)) return false;
+        }
+        return true;
+    }
+
+    private static bool IsMultipleOfNthPrimeFactor(int candidate, int n)
+    {
+        return candidate == SmallestOddNthMultipleNotLessThanCandidate(candidate, n);
+    }
+
+    private static int SmallestOddNthMultipleNotLessThanCandidate(int candidate, int n)
+    {
+        int multiple = _multiplesOfPrimeFactors[n];
+        while (multiple < candidate)
+            multiple += 2 * _primes[n];
+        _multiplesOfPrimeFactors[n] = multiple;
+        return multiple;
+    }
 }
