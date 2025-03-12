f9976c3a1b101aefe41b607ebcfe6eb510aacfe6
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-03-12 12:09:51 -0400

    Finish BankProxyHandler implementation

--- Systems/Proxies/BankProxyHandler.cs
+++ Systems/Proxies/BankProxyHandler.cs
@@ -10,7 +10,26 @@ public class BankProxyHandler : MarshalByRefObject
         _bank = bank;
     }
 
-    // ...
+    // Method defined in InvocationHandler in Java (or here in MarshalByRefObject)
+    public object Invoke(object proxy, Method method, object[] args) 
+    {
+        string methodName = method.GetName();
+        if (methodName.Equals("GetAccounts"))
+        {
+            _bank.SetAccounts(GetAccountsFromDatabase());
+            return _bank.GetAccounts();
+        } else if (methodName.Equals("SetAccounts"))
+        {
+            _bank.SetAccounts((IList<Account>)args[0]);
+            SetAccountsToDatabase(_bank.GetAccounts());
+            return null;
+        } else 
+        {
+            ...
+        }
+    }
 
-    
+    // Lots of details here:
+    protected IList<Account> GetAccountsFromDatabase() { ... }
+    protected void SetAccountsToDatabase(IList<Account> accounts) { ... }
 }
