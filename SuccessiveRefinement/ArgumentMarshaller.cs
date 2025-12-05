40671bac23599ccff30c9ff243f4b9a72820d6b0
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-12-05 15:45:34 -0500

    Add string value handling to ArgumentMarshaller

--- SuccessiveRefinement/ArgumentMarshaller.cs
+++ SuccessiveRefinement/ArgumentMarshaller.cs
@@ -1,8 +1,13 @@
 private class ArgumentMarshaller
 {
     private bool _boolValue = false;
+    private string _stringValue;
 
     public void SetBool(bool value) { _boolValue = value; }
 
     public bool GetBool() { return _boolValue; }
+
+    public void SetString(string s) { _stringValue = s; }
+
+    public string GetString() { return _stringValue == null ? string.Empty : _stringValue; }
 }
