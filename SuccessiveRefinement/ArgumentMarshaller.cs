8fa86796e8a4b47adbeef83878081f9aca60ef27
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-12-06 13:43:37 -0500

    Refactor ArgumentMarshaller by removing integer methods

--- SuccessiveRefinement/ArgumentMarshaller.cs
+++ SuccessiveRefinement/ArgumentMarshaller.cs
@@ -1,12 +1,5 @@
 private abstract class ArgumentMarshaller
 {  
-    private int _integerValue;
-
-    public void SetInteger(int i) { _integerValue = i; }
-
-    public int GetInteger() { return _integerValue; }
-
     public abstract void Set(string s);
-
     public abstract object Get();
 }
