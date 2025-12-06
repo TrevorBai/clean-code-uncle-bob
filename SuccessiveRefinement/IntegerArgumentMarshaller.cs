cfdca5e0184af499324886435561b623fd4cfd47
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-12-06 13:49:01 -0500

    Implement integer parsing in IntegerArgumentMarshaller

--- SuccessiveRefinement/IntegerArgumentMarshaller.cs
+++ SuccessiveRefinement/IntegerArgumentMarshaller.cs
@@ -1,6 +1,18 @@
 private class IntegerArgumentMarshaller : ArgumentMarshaller
 {
-    public override void Set(string s) { }
+    private int _integerValue = 0;
 
-    public override object Get() { return null; }
+    public override void Set(string s) 
+    { 
+        try
+        {
+            _integerValue = int.Parse(s);
+        }
+        catch (FormatException e)
+        {
+            throw new ArgsException();
+        } 
+    }
+
+    public override object Get() { return _integerValue; }
 }
