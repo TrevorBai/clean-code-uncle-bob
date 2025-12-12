839fd66c196bd0b2f091c4cf63c567ce37dd1786
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-12-11 21:38:04 -0500

    Update BoolArgumentMarshaller.cs

--- SuccessiveRefinement/BoolArgumentMarshaller.cs
+++ SuccessiveRefinement/BoolArgumentMarshaller.cs
@@ -2,7 +2,9 @@ private class BoolArgumentMarshaller : ArgumentMarshaller
 {
     private bool _boolValue = false;
 
-    public override void Set(string s) { _boolValue = true; }
+    public override void Set(IEnumerator<string> argsIterator) { _boolValue = true; }
+
+    public override void Set(string s) { }
 
     public override object Get() { return _boolValue; }
 }
