b92206fa9767c27baa02d94f0939256caf361604
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-12-11 21:53:21 -0500

    Update StringArgumentMarshaller.cs

--- SuccessiveRefinement/StringArgumentMarshaller.cs
+++ SuccessiveRefinement/StringArgumentMarshaller.cs
@@ -2,9 +2,21 @@ private class StringArgumentMarshaller : ArgumentMarshaller
 {
     private string _stringValue = "";
 
-    public override void Set(IEnumerator<string> argsIterator) { }
+    public override void Set(IEnumerator<string> argsIterator) 
+    {
+        try
+        {
+            argsIterator.MoveNext();     
+            _stringValue = _argsIterator.Current;
+        }
+        catch (InvalidOperationException e)
+        {
+            _errorCode = ErrorCode.MISSING_STRING;
+            throw new ArgsException();
+        }
+    }
 
-    public override void Set(string s) { _stringValue = s; }
+    public override void Set(string s) { }
 
     public override object Get() { return _stringValue; }
 }
