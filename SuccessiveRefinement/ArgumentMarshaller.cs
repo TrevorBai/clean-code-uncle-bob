4e5609b03840fa056fe99170e6951401afaa0bad
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-12-11 21:36:15 -0500

    Add another Set method taking an iterator of strings

--- SuccessiveRefinement/ArgumentMarshaller.cs
+++ SuccessiveRefinement/ArgumentMarshaller.cs
@@ -1,5 +1,6 @@
 private abstract class ArgumentMarshaller
-{  
+{
+    public abstract void Set(IEnumerator<string> argsIterator);
     public abstract void Set(string s);
     public abstract object Get();
 }
