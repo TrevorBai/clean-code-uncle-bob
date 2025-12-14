d905ad90a4b02f0956153e3c20251979188cfc07
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-12-13 19:43:12 -0500

    Get rid of unused method

--- SuccessiveRefinement/ArgumentMarshaller.cs
+++ SuccessiveRefinement/ArgumentMarshaller.cs
@@ -1,6 +1,5 @@
 private abstract class ArgumentMarshaller
 {
     public abstract void Set(IEnumerator<string> argsIterator);
-    public abstract void Set(string s);
     public abstract object Get();
 }
