7d2acc853b77100ebccd87e6d07fd981e2a1eb9a
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-12-15 22:30:10 -0500

    Add TestInvalidDouble test method

--- SuccessiveRefinement/Test.cs
+++ SuccessiveRefinement/Test.cs
@@ -6,3 +6,14 @@ public void TestSimpleDoublePresent()
     AssertTrue(args.Has('x'));
     AssertEquals(42.3, args.GetDouble('x'), .001);
 }
+
+public void TestInvalidDouble()
+{
+    var args = new Args("x##", new string[] {"-x", "Forty two"});
+    AssertFalse(args.IsValid());
+    AssertEquals(0, args.Cardinality());
+    AssertFalse(args.Has('x'));
+    AssertEquals(0, args.GetInt('x'));
+    AssertEquals("Argument -x expects a double but was \"Forty two\".",
+                args.ErrorMessage());   
+}
