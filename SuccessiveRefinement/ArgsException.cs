4346555493ef2cbb0816e2a0ba99f7887355db56
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2025-12-17 20:56:00 -0500

    Migrate certain code from Args.cs into this class

--- SuccessiveRefinement/ArgsException.cs
+++ SuccessiveRefinement/ArgsException.cs
@@ -1,6 +1,21 @@
     public class ArgsException : Exception
     {
+        private char _errorArgumentId = '\0';
+        private string _errorParameter = "TILT";
+        private ErrorCode _errorCode = ErrorCode.OK;
 
+        public ArgsException() { }
 
-        
+        public ArgsException(string message) : base(message) { }
+
+        public enum ErrorCode
+        {
+            OK,
+            MISSING_STRING,
+            MISSING_INTEGER,
+            INVALID_INTEGER,
+            UNEXPECTED_ARGUMENT,
+            MISSING_DOUBLE,
+            INVALID_DOUBLE
+        }
     }
