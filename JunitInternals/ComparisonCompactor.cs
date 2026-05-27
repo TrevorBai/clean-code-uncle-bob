3c073fd8e7115ed9183efe44111953524b4ce529
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2026-05-27 00:53:52 -0400

    Remove f prefix for member variables

--- JunitInternals/ComparisonCompactor.cs
+++ JunitInternals/ComparisonCompactor.cs
@@ -4,72 +4,72 @@ public class ComparisonCompactor
     private static const string DELTA_END = "]";
     private static const string DELTA_START = "[";
 
-    private int _fContextLength;
-    private string _fExpected;
-    private string _fActual;
-    private int _fPrefix;
-    private int _fSuffix;
+    private int _contextLength;
+    private string _expected;
+    private string _actual;
+    private int _prefix;
+    private int _suffix;
 
     public ComparisonCompactor(int contextLength, string expected, string actual)
     {
-        _fContextLength = contextLength;
-        _fExpected = expected;
-        _fActual = actual;
+        _contextLength = contextLength;
+        _expected = expected;
+        _actual = actual;
     }
 
     public string Compact(string message)
     {
-        if (_fExpected == null || _fActual == null || AreStringsEqual())
-            return Assert.Format(message, _fExpected, _fActual);
+        if (_expected == null || _actual == null || AreStringsEqual())
+            return Assert.Format(message, _expected, _actual);
 
         FindCommonPrefix();
         FindCommonSuffix();
-        string expected = CompactString(_fExpected);
-        string actual = CompactString(_fActual);
+        string expected = CompactString(_expected);
+        string actual = CompactString(_actual);
         return Assert.Format(message, expected, actual);
     }
 
     private string CompactString(string source)
     {
-        string result = DELTA_START + source.Substring(_fPrefix, source.Length - _fSuffix + 1) + DELTA_END;
-        if (_fPrefix > 0) result = ComputCommonPrefix() + result;
-        if (_fSuffix > 0) result = result + ComputeCommonSuffix();
+        string result = DELTA_START + source.Substring(_prefix, source.Length - _suffix + 1) + DELTA_END;
+        if (_prefix > 0) result = ComputCommonPrefix() + result;
+        if (_suffix > 0) result = result + ComputeCommonSuffix();
         return result;
     }
 
     private void FindCommonPrefix()
     {
-        _fPrefix = 0;
-        int end = Math.Min(_fExpected.Length, _fActual.Length);
-        for (; _fPrefix < end; _fPrefix++)
+        _prefix = 0;
+        int end = Math.Min(_expected.Length, _actual.Length);
+        for (; _prefix < end; _prefix++)
         {
-            if (_fExpected[_fPrefix] != _fActual[_fPrefix]) break;
+            if (_expected[_prefix] != _actual[_prefix]) break;
         }   
     }
 
     private void FindCommonSuffix()
     {
-        int expectedSuffix = _fExpected.Length - 1;
-        int actualSuffix = _fActual.Length - 1;
-        for (; actualSuffix >= _fPrefix && expectedSuffix >= _fPrefix; actualSuffix--, expectedSuffix--)
+        int expectedSuffix = _expected.Length - 1;
+        int actualSuffix = _actual.Length - 1;
+        for (; actualSuffix >= _prefix && expectedSuffix >= _prefix; actualSuffix--, expectedSuffix--)
         {
-            if (_fExpected[_expectedSuffix] != _fActual[_actualSuffix]) break;
+            if (_expected[_expectedSuffix] != _actual[_actualSuffix]) break;
         }
-        _fSuffix = _fExpected.Length - expectedSuffix;   
+        _suffix = _expected.Length - expectedSuffix;   
     }
 
     private string ComputeCommonPrefix()
     {
-        return (_fPrefix > _fContextLength ? ELLIPSIS : "") + _fExpected.Substring(Math.Max(0, _fPrefix - _fContextLength), _fPrefix);       
+        return (_prefix > _contextLength ? ELLIPSIS : "") + _expected.Substring(Math.Max(0, _prefix - _contextLength), _prefix);       
     }
 
     private string ComputeCommonSuffix()
     {
-        int end = Math.Min(_fExpected.Length - _fSuffix + 1 + _fContextLength, _fExpected.Length);
-        return _fExpected.Substring(_fExpected.Length - _fSuffix + 1, end) + 
-            (_fExpected.Length - _fSuffix + 1 < _fExpected.Length - _fContextLength ? ELLIPSIS : "");
+        int end = Math.Min(_expected.Length - _suffix + 1 + _contextLength, _expected.Length);
+        return _expected.Substring(_expected.Length - _suffix + 1, end) + 
+            (_expected.Length - _suffix + 1 < _expected.Length - _contextLength ? ELLIPSIS : "");
     }
 
-    private bool AreStringsEqual() { return _fExpected.Equals(_fActual); }
+    private bool AreStringsEqual() { return _expected.Equals(_actual); }
     
 }
