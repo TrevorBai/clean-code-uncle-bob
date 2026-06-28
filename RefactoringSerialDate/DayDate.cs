f5e058efdd4bb5cecf1d50bdaf7abbdb65ed014d
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2026-06-27 23:08:06 -0400

    Refactor: Remove Month enum and MonthHelper class
    Removed Month enum and MonthHelper class from DayDate.

--- RefactoringSerialDate/DayDate.cs
+++ RefactoringSerialDate/DayDate.cs
@@ -1,22 +1,6 @@
 [Serializable]
 public abstract class DayDate : IComparable<DayDate>, IEquatable<DayDate>
 {
-    public static enum Month : byte
-    {
-        January = 1,
-        February = 2,
-        March = 3,
-        April = 4,
-        May = 5,
-        June = 6,
-        July = 7,
-        August = 8,
-        September = 9,
-        October = 10,
-        November = 11,
-        December = 12
-    }
-
     public enum WeekInMonth
     {
         First = 1,
@@ -25,17 +9,6 @@ public abstract class DayDate : IComparable<DayDate>, IEquatable<DayDate>
         Fourth = 4,
         Last = 0
     }
-    
-    public static class MonthHelper
-    {
-        public static Month FromIndex(int monthIndex)
-        {
-            if (monthIndex < 1 || monthIndex > 12)
-                throw new ArgumentException($"Invalid month index {monthIndex}");
-            return (Month)monthIndex;
-        }       
-    }
-
     
     public abstract int CompareTo(DayDate other);
     public abstract bool Equals(DayDate other);
